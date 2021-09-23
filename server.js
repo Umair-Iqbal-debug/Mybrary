f82def4171c72294eee66672b3712b03419a34b7
Umair-Iqbal-Debug <iqbalumairi@gmail.com>
2021-09-23 19:14:45 -0400

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/author");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -17,6 +21,7 @@ app.use(express.static("public"));
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
